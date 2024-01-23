3eea77026822b8565bc420776980e02f41e3883f
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2024-01-23 22:29:50 +0200

    completed added the global css module

---
+++ worldwise/src/components/AppLayout.jsx
@@ -0,0 +1,9 @@
+import AppNav from "./AppNav";
+
+export default function AppLayOut() {
+  return (
+    <div>
+      <AppNav />
+    </div>
+  );
+}
--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
+import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+      <PageNav />
       <section>
         <h1>
           You travel the world.
@@ -15,6 +18,7 @@ export default function Homepage() {
           you have wandered the world.
         </h2>
       </section>
+      <Link to="applayout">Got to the App</Link>
     </main>
   );
 }
