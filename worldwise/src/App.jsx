ea2ad058d0a2a70380a16b9028d7a8d935739989
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2024-01-28 05:51:57 +0200

    completed adding the context API

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -6,69 +6,36 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayOut from "./components/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+
 import CountryList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:9000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-
-        setCities(data);
-      } catch {
-        alert("There was an Error Loading Data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchCities();
-    console.log(cities);
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="product" element={<Product />} />
-        <Route path="applayout" element={<AppLayOut />}>
-          <Route
-            index
-            element={
-              <CityList
-                cities={cities}
-                element={<Navigate to="cities" />}
-                isLoading={isLoading}
-              />
-            }
-          />
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="cities/:id" element={<City />} />
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="applayout" element={<AppLayOut />}>
+            <Route
+              index
+              element={<CityList element={<Navigate to="cities" />} />}
+            />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
 
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
---
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { createContext } from "react";
+
+const BASE_URL = "http://localhost:9000";
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch {
+        alert("There was an Error Loading Data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+    console.log(cities);
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
