51fc30a37d6d608e6c2df1a983b5158a2c1c0e5b
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2024-01-30 01:41:50 +0200

    added  logics to add a new city based on where the user clicks on the map

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -12,20 +12,20 @@ import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import useGeolocation from "../hooks/useGeolocation";
 import Button from "./Button";
+import useUrlPosition from "../hooks/useUrlPosition";
 
 export default function Map() {
   const navigate = useNavigate();
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
+
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
