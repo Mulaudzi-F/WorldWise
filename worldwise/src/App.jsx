01f71cb868150cef4182dd8188c583a565b5aa7b
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2024-01-31 01:53:46 +0200

    completed adding the fake authentication

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -11,31 +11,34 @@ import CountryList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthoContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="product" element={<Product />} />
-          <Route path="applayout" element={<AppLayOut />}>
-            <Route
-              index
-              element={<CityList element={<Navigate to="cities" />} />}
-            />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="product" element={<Product />} />
+            <Route path="applayout" element={<AppLayOut />}>
+              <Route
+                index
+                element={<CityList element={<Navigate to="cities" />} />}
+              />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
 
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
--- worldwise/src/components/AppLayout.jsx
+++ worldwise/src/components/AppLayout.jsx
@@ -2,12 +2,14 @@ import AppNav from "./AppNav";
 import Sidebar from "./Sidebar";
 import Map from "./Map";
 import styles from "./AppLayout.module.css";
+import User from "./User";
 
 export default function AppLayOut() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
